31b7d9fc4cf8d398073fd90b164c727273787fb4
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-12 15:00:25 +0530

    commit

---
+++ Basics/EvenOdd.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class EvenOdd {
+    public static void main(String args[]) {
+        int num = 19;
+        if (num % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
