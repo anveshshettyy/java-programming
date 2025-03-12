3603afb86e54dc69634a38af90881522d118597a
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-12 15:08:18 +0530

    commit

--- Basics/IsYounger.java
+++ Basics/IsYounger.java
@@ -3,6 +3,6 @@ package Basics;
 public class IsYounger {
     public static void main(String args[]) {
         int age = 28;
-        System.out.println((age >= 18) ? "Adult" : "Teenage");
+        System.out.println((age >= 18) ? "Older" : "Younger");
     }
 }
