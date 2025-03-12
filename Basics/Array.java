0fbf7faeed9d03350b691d1ba9505ced4b5b2029
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-12 10:09:04 +0530

    commit

---
+++ Basics/Array.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Array {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        nums[0] = 10;
+
+        System.out.println(nums[0]);
+    }
+}
