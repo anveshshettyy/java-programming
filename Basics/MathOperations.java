e20c6e3d6956cbad5c717c1ea056799b7379ca1e
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-12 15:00:35 +0530

    commit

---
+++ Basics/MathOperations.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class MathOperations {
+    public static void main(String args[]) {
+       int a = 10;
+       int b = 5;
+       
+       int sum = a + b;
+       int prod = a * b;
+       int div = a / b;
+
+       System.out.println("Sum is: "+sum);
+       System.out.println("Product is: "+prod);
+       System.out.println("Quotient is: "+div);
+    }
+}
