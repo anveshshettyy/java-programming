43b20ff73014591a196ed733d70826c6601a5e64
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-13 12:39:17 +0530

    commit

---
+++ Basics/SimpleIntrest.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class SimpleIntrest {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principle: ");
+        float p = sc.nextInt();
+        
+        System.out.print("Enter Time: ");
+        float t = sc.nextInt();
+
+        System.out.print("Enter Rate of Intrest: ");
+        float r = sc.nextInt();
+
+        System.out.println("Enter Rate of Intrest: "+((p*t*r)/100));
+        
+    }
+}
