4008de1d6f4c5201fd6107e82d8a60135343f6cc
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-13 14:49:19 +0530

    commit

---
+++ Exercise2/Largest.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class Largest {
+    public static void main(String args[]) {
+        int num = 1234;
+
+        num = num % 10;
+
+        System.out.println(num);
+    }
+}
---
+++ Exercise2/RemoveLarge.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class RemoveLarge {
+    public static void main(String args[]) {
+        int num = 1234;
+
+        int last = num / 10;
+
+        System.out.println(last);
+    }
+}
