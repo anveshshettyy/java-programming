670d9a75029da497346d689c2b884c60cebc1d22
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-13 12:21:24 +0530

    commit

---
+++ Basics/FindArea.java
@@ -0,0 +1,62 @@
+package Basics;
+
+import java.util.Scanner;
+
+class Area {
+    public double Triangle(double h , double x) {
+        double area = 0.5 * h * x;
+        return area;
+    }
+
+    public float Square(float s) {
+        float area = s * s;
+        return area;
+    }
+    public float Rectangle(float l, float b) {
+        float area = l * b;
+        return area;
+    }
+}
+
+public class FindArea {
+    public static void main(String args[]) {
+        Area obj = new Area();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter height: ");
+        float h = sc.nextFloat();
+
+        System.out.println("Enter breadth: ");
+        float b = sc.nextFloat();
+        double triangleArea = obj.Triangle(h,b);
+
+        System.out.println("Enter side: ");
+        float a = sc.nextFloat();
+        double SquareArea = obj.Square(a);
+
+        System.out.println("Area of Triangle is: "+triangleArea);
+        System.out.println("Area of Square is: "+SquareArea);
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Height: ");
+        // float h = sc.nextFloat();
+        // System.out.println("Enter Base: ");
+        // float B = sc.nextFloat();
+        // double triangle = (0.5 * h * B);
+        // System.out.println("Area of Triangle is: "+triangle);
+
+        // System.out.println("Enter length: ");
+        // float l = sc.nextFloat();
+        // System.out.println("Enter Breadth: ");
+        // float b = sc.nextFloat();
+        // double rectangle = (l * b);
+        // System.out.println("Area of Rectangle is: "+rectangle);
+
+        // System.out.println("Enter Side: ");
+        // float s = sc.nextFloat();
+        // double square = (s * s);
+        // System.out.println("Area of Square is: "+square);
+
+    }
+}
