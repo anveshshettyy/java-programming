50ad1406596ef3425b03d195645cce5cd1e2a716
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-15 12:20:15 +0530

    commit

---
+++ Methods/Personal2.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Personal2 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            System.out.print('0');
+        }
+        System.out.print(n);
+    }
+}
