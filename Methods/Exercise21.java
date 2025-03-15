8a9e8ced96beb8b39554fb65a72af3501d1abdef
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-15 12:32:14 +0530

    commit

---
+++ Methods/Exercise21.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class Exercise21 {
+    static int Mul(int a) {
+        int rem = 0,mul = 0;
+        while (a > 0) {
+            rem = a % 10;
+            a /= 10;
+            for (int i = 1; i <= rem; i++)  mul = mul + i;
+        }
+        return mul;
+    }
+
+    public static void main(String[] args) {
+        int n = 125369;
+        System.out.println(Mul(n));
+    }
+}
