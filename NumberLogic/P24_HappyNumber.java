853795b22896fc1933717d46f4f024b1765f8670
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-16 11:39:14 +0530

    commit

---
+++ NumberLogic/P24_HappyNumber.java
@@ -0,0 +1,26 @@
+package NumberLogic;
+
+import java.util.Scanner;
+
+public class P24_HappyNumber {
+    static int HappyNumber(int n) {
+        int sum = 0;
+        while(n>0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += rem*rem;
+        }
+        return (sum < 10) ? sum : HappyNumber(sum);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int res = HappyNumber(n);
+
+        System.out.println((res == 1 || res == 7) ? "Happy Number" : "Not a Happy Number");
+
+    }
+}
