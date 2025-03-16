d8a46e2431f68c09748fcc7389ff7b9d8ec4d874
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-16 11:38:55 +0530

    commit

---
+++ NumberLogic/P21_FriendlyPair.java
@@ -0,0 +1,33 @@
+package NumberLogic;
+
+import java.util.Scanner;
+
+public class P21_FriendlyPair {
+    static int FriendlyPair (int n) {
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        int res1 = (FriendlyPair (a));
+        System.out.println("Friendly Ratio: "+res1/a);
+
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+
+        
+        int res2 = (FriendlyPair (b));
+        System.out.println("Friendly Ratio: "+res2/b);
+
+        System.out.println((res1/a == res2/b) ? "It is a Fiendly Pair " : "It is Not a Friendly Pair" );   
+    }
+}
