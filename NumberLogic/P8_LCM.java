d60f96bf879f4d1089eb768970fe08e088283edd
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-17 16:07:18 +0530

    commit

--- NumberLogic/P8_LCM.java
+++ NumberLogic/P8_LCM.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class P8_LCM {
     static int LCM(int a, int b) {
         int less = (a < b) ? a : b;
-
         int i;
         for(i=2; i<less; i++) {
             if((a%i == 0) && (b%i == 0)) break;
         }
-
         return i;
     }
 
