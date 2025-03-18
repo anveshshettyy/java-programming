577c7fdea0613c41ae4f4ba0c970e269c88de692
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-18 22:11:34 +0530

    commit

--- NumberLogic/P91_CircularPrime.java
+++ NumberLogic/P91_CircularPrime.java
@@ -3,20 +3,29 @@ package NumberLogic;
 import java.util.Scanner;
 
 public class P91_CircularPrime {
-    static void CyclicPrime(int n) {
+    static int RotateNumber(int n) {
+        int first = 0;
+        int rem = 0;
+
+        first = n%10;
+        rem = n / 10;
+
+        return rem;
+    }
+    static int isPrime(int n) {
         int flag = 0;
-        while (n > 0) {
-            int rem = n % 10;
-            n /= 10;
-            if (rem == 0 || rem == 2 || rem == 4 || rem == 5 || rem == 6 || rem == 8) {
+        for (int i = 2; i <=n/2; i++) {
+            if (n % i == 0) {
                 flag = 1;
+                break;
             }
         }
-        if (flag == 0) {
-            System.out.println("It is a Circular Prime");
-        } else {
-            System.out.println("It is not a Circular Prime");
-        }
+        return (flag == 1) ? 1 : 0;
+    }
+
+    static void CircularPrime(int n) {
+        
+
     }
 
     public static void main(String[] args) {
@@ -25,12 +34,8 @@ public class P91_CircularPrime {
         System.out.print("Enter number: ");
         int num = sc.nextInt();
 
-        if (num == 2 || num == 3 || num == 5 || num == 7) {
-            System.out.println("It is a Circular Prime");
-        } else {
-            CyclicPrime(num);
-
-        }
+        // CircularPrime(num);
+        System.out.println(RotateNumber(num));
 
     }
 }
