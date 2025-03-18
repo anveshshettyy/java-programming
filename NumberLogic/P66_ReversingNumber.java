c8e33d8a0e5aa7db27a6a293d9ce69fa236326e6
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-18 22:10:56 +0530

    commit

--- NumberLogic/P66_ReversingNumber.java
+++ NumberLogic/P66_ReversingNumber.java
@@ -3,15 +3,16 @@ package NumberLogic;
 import java.util.Scanner;
 
 public class P66_ReversingNumber {
-    static int Reverse(int n) {
-        return n;
+    static int Reverse(int n, int res) {
+        if(n==0) return res;
+        int rem = n%10;
+        return Reverse(n/10, res*10 + rem);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number to reverse: ");
         int n = sc.nextInt();
 
-        int reverse = Reverse(n);
-        System.out.println(reverse);
+        System.out.println(Reverse(n,0));
     }
 }
