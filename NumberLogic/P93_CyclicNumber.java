109661c01ed9b4cfa99f9324164cac8089332b2f
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-18 22:11:24 +0530

    commit

---
+++ NumberLogic/P93_CyclicNumber.java
@@ -0,0 +1,12 @@
+package NumberLogic;
+
+import java.util.Scanner;
+
+public class P93_CyclicNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        
+    }
+}
