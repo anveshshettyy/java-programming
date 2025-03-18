f9b03e25db3b31a31e3c6460fd454de107a2eb57
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-18 22:11:19 +0530

    commit

--- NumberLogic/P69_Sum.java
+++ NumberLogic/P69_Sum.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class P69_Sum {
     static int Sum(int a, int b) {
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-        return b;
+        while(b!=0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry<<1;
+        }
+        return a;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a: ");
+        System.out.print("Enter a: ");
         int a = sc.nextInt();
 
-        System.out.println("Enter b: ");
+        System.out.print("Enter b: ");
         int b = sc.nextInt();
 
         System.out.println(Sum(a,b)); 
