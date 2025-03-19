3f6ae2d2d87699273da941188251fe877c7f56e0
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-19 16:12:25 +0530

    commit

--- BitManipulation/Clear_ithBit.java
+++ BitManipulation/Clear_ithBit.java
@@ -1,5 +1,22 @@
 package BitManipulation;
 
+import java.util.Scanner;
+
 public class Clear_ithBit {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter i: ");
+        int i = sc.nextInt();
+        
+        int res = n;
+        // int temp = 0;
+        // temp = 1<<i;
+
+        if(((res>>i) & 1 ) == 1) res = n ^ 1<<i;
+
+        System.out.println(res);
+    }
 }
