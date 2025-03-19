01df2234e158a8ec8c3ad4f8d199d463504be69d
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-19 16:12:33 +0530

    commit

--- BitManipulation/Toggle_ithBit.java
+++ BitManipulation/Toggle_ithBit.java
@@ -1,5 +1,16 @@
 package BitManipulation;
 
+import java.util.Scanner;
+
 public class Toggle_ithBit {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter i: ");
+        int i = sc.nextInt();
+
+        System.out.println(n ^( 1<<i ));
+    }
 }
