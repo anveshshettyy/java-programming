3184d9f4eca8af119feef5a8861be67ffa765ca8
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-21 09:30:44 +0530

    commit

--- NumberLogic/P78_RandomInteger.java
+++ NumberLogic/P78_RandomInteger.java
@@ -1,5 +1,24 @@
 package NumberLogic;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class P78_RandomInteger {
-    
+    static void RandomNums(int min, int max, int count) {
+        Random rand = new Random();
+        for(int i=0; i<count; i++) {
+            System.out.print((rand.nextInt(max - min + 1) + min)+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Min: ");
+        int min = sc.nextInt();
+        System.out.println("Enter Max: ");
+        int max = sc.nextInt();
+        System.out.println("Enter Count: ");
+        int count = sc.nextInt();
+
+        RandomNums(min, max, count);
+    }
 }
