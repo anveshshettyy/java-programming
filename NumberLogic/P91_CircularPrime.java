a77f546a72da6d034853032446c1aad7b8aee089
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-22 12:11:51 +0530

    commit

--- NumberLogic/P91_CircularPrime.java
+++ NumberLogic/P91_CircularPrime.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class P91_CircularPrime {
     static int RotateNumber(int n) {
-        int first = 0;
-        int rem = 0;
-
-        first = n%10;
-        rem = n / 10;
-
-        return rem;
+        int temp = n, pow = 1;
+        while(temp>0) {
+            pow *= 10;
+            temp /= 10;
+        }
+        pow /= 10;
+        int first = n%10;
+        int last = n/10;
+        
+        System.out.println(first*pow + last);
+        return (first*pow + last);
     }
+
     static int isPrime(int n) {
         int flag = 0;
         for (int i = 2; i <=n/2; i++) {
@@ -23,9 +28,22 @@ public class P91_CircularPrime {
         return (flag == 1) ? 1 : 0;
     }
 
-    static void CircularPrime(int n) {
-        
-
+    static int CircularPrime(int n) {
+        int temp = n, len = 0, flag = 0;
+        while(temp>0) {
+            len++;
+            temp /= 10;
+        }
+        int rotate = n;
+        for(int i=0; i<len; i++) {
+            if(isPrime(rotate)==0) {
+                rotate = RotateNumber(rotate);
+            } else{
+                flag = 1;
+                break;
+            } 
+        }
+        return (flag == 1) ? 1 : 0;
     }
 
     public static void main(String[] args) {
@@ -34,8 +52,8 @@ public class P91_CircularPrime {
         System.out.print("Enter number: ");
         int num = sc.nextInt();
 
-        // CircularPrime(num);
-        System.out.println(RotateNumber(num));
-
+        int res = CircularPrime(num);
+        System.out.println((res == 1) ? "It is not Circular Prime" : "It is Circular Prime");
+ 
     }
 }
