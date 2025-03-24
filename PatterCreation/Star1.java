93a289634f42acc59970c5a86a6c392fb9d27b48
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-24 11:02:52 +0530

    commit

--- PatterCreation/Star1.java
+++ PatterCreation/Star1.java
@@ -2,20 +2,21 @@ package PatterCreation;
 
 public class Star1 {
     public static void main(String[] args) {
-        int n=5;
+        int n=10;
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 // if( i-j == 0 ) {
                 // if(i+j == n-1) {
                 // if(i-j == 0 || i+j == n-1) {
-                if(i==n/2 && j==n/2) {
+                // if(i==n/2 && j==n/2) {
                 // if(i==0 || j==0 || i==n-1 || j==n-1) {
                 // if(j==0 || i==n-1 || i-j == 0) {
                 // if(i-j==0 || i==0 || j==n-1) {
                 // if(i==n/2 || j==n/2) {
-                    System.out.print("*");
+                if(((i==n/2)&&(j>=n/4)&&(j<=3*n/4)) || j==n-1 ) {
+                    System.out.print("* ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
