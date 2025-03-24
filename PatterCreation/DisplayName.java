16ade6339433b4bc3bf47d7e6891bb776d1148e7
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-24 11:02:39 +0530

    commit

--- PatterCreation/DisplayName.java
+++ PatterCreation/DisplayName.java
@@ -2,69 +2,60 @@ package PatterCreation;
 
 public class DisplayName {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 6;
             for(int i=0; i<n; i++) {
                 for(int j=0; j<n; j++) {
                     System.out.print((i == 0 || i == n - 1 || i == n / 2 || ((j == 0) && (i < n / 2))
                                 || ((j == n - 1) && (i > n / 2))) ? "* " : "  ");
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print(
                                 (j == 0 || i + j == n / 2 || i - j == n / 2)
                                         ? "* "
                                         : "  ");
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print((i == 0 || i == n - 1 || j == n / 2) ? "* " : "  ");
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print((j == 0 || i == n - 1) ? "* " : "  ");
 
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print((j == 0 || i == n - 1) ? "* " : "  ");
 
                 }
-                System.out.println();
-            }
+                System.out.print("   ");
+            
 
-            for(int i=0; i<n; i++) {
                 for(int j=0; j<n; j++) {
                     System.out.print((i==0 || ((i==n-1)&&(j<n*3/4 -1)) || j==0 || ((i+j==n-1)&&(i<n/2)) || i+j==3*n/2 || i==n/2) ? "* " : "  ");
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print((i == 0 || j == 0 || j == n - 1 || i == n - 1) ? "* " : "  ");
 
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print(
                                 (i == n - 1 || j == 0 || j == n - 1) ? "* "
                                         : "  ");
                 }
-                System.out.println();
-            }
-            for(int i=0; i<n; i++) {
+                System.out.print("   ");
+            
                 for(int j=0; j<n; j++) {
                     System.out.print((i == 0 || j == n / 2) ? "* " : "  ");
-
                 }
                 System.out.println();
             }
