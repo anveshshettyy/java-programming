115679e0bd06644a9e1303213fa28468e2d40248
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-24 11:02:34 +0530

    commit

--- NumberLogic/P176_SwapNibbles.java
+++ NumberLogic/P176_SwapNibbles.java
@@ -1,5 +1,17 @@
 package NumberLogic;
 
+import java.util.Scanner;
+
 public class P176_SwapNibbles {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int temp = n;
+        int i=0;
+        while(i>4) {
+            
+        }
+    }
 }
