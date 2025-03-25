1839447e63b2b3e1f11e54c79a5b09821047232b
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-25 19:46:35 +0530

    commit

---
+++ Arrays/Swap/P_9_2.java
@@ -0,0 +1,28 @@
+package Arrays.Swap;
+
+public class P_9_2 {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] arr = {3, 6, 2, 9, 7, 4};
+        int res[] = new int[6];
+
+        int j = 0;
+        for(int i=0; i<n; i++) {
+            if(arr[i] % 2 == 0) {
+                res[j] = arr[i];
+                j++;
+            } 
+        }
+
+        for(int i=0; i<n; i++) {
+            if(arr[i] % 2 != 0) {
+                res[j] = arr[i];
+                j++;
+            } 
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.print(res[i]+"  ");
+        }
+    }
+}
---
+++ Arrays/Swap/Twopointer.java
@@ -0,0 +1,29 @@
+package Arrays.Swap;
+
+public class Twopointer {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] arr = {3, 6, 2, 9, 7, 4};
+        
+        int start = 0, end = n-1;
+        while(start<end) {
+            if(arr[start] %2 != 0 && arr[end] %2 == 0) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+            else if(arr[start] %2 == 0) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i]+"  ");
+        }
+    }
+}
