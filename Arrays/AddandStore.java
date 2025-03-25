8bdcefda3d41735241022905cde1652358537c1f
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-25 14:16:13 +0530

    commmit

---
+++ Arrays/AddandStore.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class AddandStore {
+    static int SumofDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;;
+            n /= 10;
+            sum += rem;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter size: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print("Enter elements of array: ");
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++) {
+            int sum = SumofDigits(arr[i]);
+            arr[i] = sum;
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
