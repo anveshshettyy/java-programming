16da7e589f5625fc44d38c96cc6bd984339b90e0
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-03-27 12:02:31 +0530

    commit

---
+++ Array_List/ArrayToArrayList.java
@@ -0,0 +1,14 @@
+package Array_List;
+
+import java.util.*;
+
+public class ArrayToArrayList {
+    public static void main(String[] args) {
+        String[] arr = {"1", "2", "3"};
+
+        List array = Arrays.asList(arr);
+
+        System.out.print(array+" ");
+
+    }
+}
