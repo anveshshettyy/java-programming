5a66f54b9afa5088035f2ac8e1ae44e1ec441c89
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-07-13 21:35:39 +0530

    commit

--- TwoD_Array/MaxOfRowNCol.java
+++ TwoD_Array/MaxOfRowNCol.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class MaxOfRowNCol {
     public static void main(String[] args) {
-        int nums[][] = {
-                       {5, 4, 3, 1, 6},
-                       {6, 8, 4, 5, 2},
-                       {8, 6, 2, 4, 9},
-                       {5, 3, 7, 5, 9},
-                       {1, 4, 2, 6, 8}};
+        int nums[][] = { {8, 4, 8, 7},
+            {7, 4, 7, 7},
+            {9, 4, 8, 7},
+            {3, 3, 3, 3} };
         
 
         int rowMax = 0, colMax = 0;
@@ -21,13 +19,24 @@ public class MaxOfRowNCol {
         //     System.out.print(colMax+" ");
         // }
 
-        for(int i=0; i<nums[0].length; i++) {
-            rowMax = 0;
-            for(int j=0; j<nums.length; j++) {
-                rowMax = Math.max(nums[j][i], rowMax);
-            }
-            System.out.print(rowMax+" ");
+        // for(int i=0; i<nums[0].length; i++) {
+        //     rowMax = 0;
+        //     for(int j=0; j<nums.length; j++) {
+        //         rowMax = Math.max(nums[j][i], rowMax);
+        //     }
+        //     System.out.print(rowMax+" ");
+        // }
+
+        // for(int j=0; j<nums.length; j++) {
+        //     rowMax = Math.max(nums[2][j], rowMax);
+        // }
+        // System.out.println(rowMax);
+
+        for(int i=0; i<nums.length; i++) {
+            colMax = Math.max(nums[i][2], colMax);
         }
+        System.out.println(colMax);
+
 
         System.out.println();
         System.out.println("----------");
