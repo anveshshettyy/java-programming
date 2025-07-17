721ca7b71d16444b5454636c62f7ae3c57d9e7ef
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-07-17 20:58:23 +0530

    commit

--- DynamicProgramming/HouseRobberTD.java
+++ DynamicProgramming/HouseRobberTD.java
@@ -1,16 +1,24 @@
 package DynamicProgramming;
 
-public class HouseRobberTD {
-    static int MaxLootArray(int arr[], int n) {
-        int dp[] = new int[n+1];
+import java.util.Arrays;
 
-        dp[0] = 0;
-        dp[1] = arr[0];
-        
-        for(int i=2; i<=n; i++) {
-            dp[i] = Math.max((arr[i-1] + dp[i-2]), dp[i-1]);
-        }
-        return dp[n];
+public class HouseRobberTD {
+    static int MaxLoot(int memo[], int nums[], int n) {
+        if(n<=0) return 0;
+        if(n==1) return nums[0];
+
+        if(memo[n] != -1) return memo[n];
+
+        int picked = nums[n-1] + MaxLoot(memo, nums, n-2);
+        int notpicked = MaxLoot(memo, nums, n-1);
+
+        return Math.max(picked, notpicked);
+
+    }
+    static int MaxLootArray(int nums[], int n) {
+        int memo[] = new int[n+1];
+        Arrays.fill(memo, -1);
+        return MaxLoot(memo, nums, n);
     }
     public static void main(String[] args) {
         int arr[] = {6, 7, 1, 3, 8, 2, 4};
